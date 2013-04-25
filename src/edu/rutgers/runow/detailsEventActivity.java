1be6bcdb615ec9526a784cd47a9aab21ed4bf708
Revan Sopher <rsopher@gmail.com>
2013-04-25 16:40:42 -0400

    removed broken imports

--- src/edu/rutgers/runow/detailsEventActivity.java
+++ src/edu/rutgers/runow/detailsEventActivity.java
@@ -6,12 +6,8 @@ import android.annotation.TargetApi;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 
 public class detailsEventActivity extends Activity{
