f045a3f305310d2aa3c873adf50b256b10bc5c01
Revan Sopher <rsopher@gmail.com>
2013-05-02 01:50:15 -0400

    added Location field to event creation

--- src/edu/rutgers/runow/createEventActivity.java
+++ src/edu/rutgers/runow/createEventActivity.java
@@ -31,12 +31,14 @@ public class createEventActivity extends Activity {
 	}
 	public void createEvent(View button){
 		final EditText nameField = (EditText)findViewById(R.id.FieldEventName);
+		final EditText locationField = (EditText)findViewById(R.id.FieldEventLocation);
 		final EditText descriptionsField = (EditText)findViewById(R.id.FieldEventDescription);
 		final DatePicker dateField= (DatePicker)findViewById(R.id.FieldDate);
 		final TimePicker timeField= (TimePicker)findViewById(R.id.FieldTime);
 		//final Spinner spinnerTags = (Spinner)findViewById(R.id.SpinnerTags);
 		
 		String name = nameField.getText().toString();
+		String location = locationField.getText().toString();
 		String description = descriptionsField.getText().toString();
 		Date when = new Date(dateField.getYear(),dateField.getMonth(),dateField.getDayOfMonth(),
 				timeField.getCurrentHour(),timeField.getCurrentMinute());
@@ -44,11 +46,6 @@ public class createEventActivity extends Activity {
 		String tag ="no tags";
 		if(tag.equals("no tags"))
 			tag="";
-		
-		String location =""; //TODO implement location
-		
-		// adjusted Event to have id and url members, but these are set by the server
-		// maybe overloaded constructor?
 		
 		//send event to server
 		HttpClient httpclient = new DefaultHttpClient();
