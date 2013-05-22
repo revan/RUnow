1d80dc370def518791a54074d072761d9310a7ad
Revan Sopher <rsopher@gmail.com>
2013-05-21 23:21:57 -0400

    Posting comments

--- src/edu/rutgers/runow/detailsEventActivity.java
+++ src/edu/rutgers/runow/detailsEventActivity.java
@@ -10,7 +10,11 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -21,6 +25,10 @@ import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -29,11 +37,13 @@ public class detailsEventActivity extends Activity{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.details_event);
 		
-		Event event = (Event) getIntent().getSerializableExtra("event");
+		final Event event = (Event) getIntent().getSerializableExtra("event");
 		
-		TextView name = (TextView)findViewById(R.id.detailsEventName);
-		TextView dateAndLocation = (TextView)findViewById(R.id.detailsEventDateLocation);
-		TextView description = (TextView)findViewById(R.id.detailsDescription);
+		final TextView name = (TextView)findViewById(R.id.detailsEventName);
+		final TextView dateAndLocation = (TextView)findViewById(R.id.detailsEventDateLocation);
+		final TextView description = (TextView)findViewById(R.id.detailsDescription);
+		
+		final EditText commentField = (EditText)findViewById(R.id.detailsCommentField);
 		
 		name.setText(event.name);
 		dateAndLocation.setText(new SimpleDateFormat("h:mm a").format(event.when) + " at " + event.location);
@@ -50,7 +60,35 @@ public class detailsEventActivity extends Activity{
 		final Comment[] comments = getComments(event.url);
 		CommentAdapter adapter = new CommentAdapter(this, comments);
 		listView.setAdapter(adapter);
-
+		
+		final Button post = (Button)findViewById(R.id.detailsCommentButton);
+		
+		post.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Log.v("RUNow Server", event.id.toString());
+				//send event to server
+				HttpClient httpclient = new DefaultHttpClient();
+				HttpPost httppost = new HttpPost(getString(R.string.url)+"/events/"+event.id+"/comments");
+				httppost.addHeader(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+				HttpResponse response;
+				JSONObject json = new JSONObject();
+				try{
+					JSONObject data = new JSONObject();
+					data.put("user","default");
+					data.put("content",commentField.getText());
+					
+					json.put("comment", data);
+					ByteArrayEntity message = new ByteArrayEntity(json.toString().getBytes("UTF8"));
+					httppost.setEntity(message);
+					response = httpclient.execute(httppost);
+					
+					}catch(Exception e){
+					Log.e("RUNow Server", e.toString());
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 	
 	private Comment[] getComments(String thisEventURL) {
